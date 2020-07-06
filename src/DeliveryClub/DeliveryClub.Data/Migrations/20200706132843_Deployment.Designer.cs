143ad2e81835f9c6a64bf5a13c9b4c67af56d386
ALLIOT21 <35888295+ALLIOT21@users.noreply.github.com>
2020-07-06 17:16:28 +0300

    Trying to deploy

--- src/DeliveryClub/DeliveryClub.Data/Migrations/20200706132843_Deployment.Designer.cs
+++ src/DeliveryClub/DeliveryClub.Data/Migrations/20200706132843_Deployment.Designer.cs
@@ -4,14 +4,16 @@ using DeliveryClub.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DeliveryClub.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200706132843_Deployment")]
+    partial class Deployment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DeliveryClub/DeliveryClub.Data/Migrations/20200706132843_Deployment.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryClub.Data.Migrations
+{
+    public partial class Deployment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentMethods");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentMethods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentMethod = table.Column<int>(type: "int", nullable: false),
+                    RestaurantAdditionalInfoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentMethods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaymentMethods_RestaurantAdditionalInfos_RestaurantAdditionalInfoId",
+                        column: x => x.RestaurantAdditionalInfoId,
+                        principalTable: "RestaurantAdditionalInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentMethods_RestaurantAdditionalInfoId",
+                table: "PaymentMethods",
+                column: "RestaurantAdditionalInfoId");
+        }
+    }
+}
